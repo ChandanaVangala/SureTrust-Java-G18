50c2c4235448802f3eec6fc7cf1efcd38b2c6a71
Chandana15 <118879299+ChandanaVangala@users.noreply.github.com>
2025-02-06 22:29:44 +0530

    Add files via upload

---
+++ SURETRUST/dayfive/Constant.java
@@ -0,0 +1,9 @@
+package dayfive;
+public class Constant
+{
+    public static void main(String args[])
+    {
+        final float PI=3.141233f;
+        System.out.println("PI value: "+ PI);
+    }
+}
---
+++ SURETRUST/dayfive/Primitives.java
@@ -0,0 +1,32 @@
+package dayfive;
+
+public class Primitives {
+    public static void main(String[] args) {
+        //numbers
+        byte a1=12;
+        short b1=522;
+        int c1=1331;
+        long d1=234567;
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        System.out.println(d1);
+
+        //floating points
+        float e1=1.123456789123456789f;//suffix f is mandatory, precision upto 8 digits.
+        double f1=1.123456789123456789;//suffix d is optional, precision upto 17 digits.
+        System.out.println(e1);
+        System.out.println(f1);
+
+        //character
+        char g1='w';
+        System.out.println(g1);
+
+        //boolean
+        boolean h1=false;
+        System.out.println(h1);
+
+        
+    }
+    
+}
